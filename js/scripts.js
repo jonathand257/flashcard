47db1b1f3248d6243e1e314bf5ba81ece91bfaac
Jonathan Caldwell <jonathan.caldwell@moringaschool.com>
2017-11-06 20:49:10 +0300

    Add JavaScript subject

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+       $(".clickable").click(function() {
+         $(".initial-showing").fadeToggle();
+         $(".initial-hidden").fadeToggle();
+       });
+     });
